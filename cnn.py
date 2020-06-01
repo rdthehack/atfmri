19045a9a5a24a39dd376c4f2a4603b5c73e39f56
Rohan Datta <rohandatt@gmail.com>
2020-06-01 20:44:38 +0530

    Update cnn.py
    Added testing and deployment capability

--- cnn.py
+++ cnn.py
@@ -11,6 +11,7 @@ import numpy as np
 import keras
 import h5py
 from sklearn.metrics import accuracy_score
+from keras.callbacks import ModelCheckpoint
 
 class FModel:    
 
@@ -22,44 +23,64 @@ class FModel:
         ytest = keras.utils.to_categorical(ytest, 2)
         self.ytrain = ytrain
         self.ytest = ytest
-        
-        ## input layer
+  
         input_layer = Input(shape)
 
-        ## convolutional layers
         conv_layer1 = Conv3D(filters=8, kernel_size=(4, 4, 4), activation='relu')(input_layer)
         conv_layer2 = Conv3D(filters=16, kernel_size=(4, 4, 4), activation='relu')(conv_layer1)
 
-        ## add max pooling to obtain the most imformatic features
         pooling_layer1 = MaxPool3D(pool_size=(2, 2, 2))(conv_layer2)
 
-        conv_layer3 = Conv3D(filters=32, kernel_size=(4, 4, 4), activation='relu')(pooling_layer1)
-        conv_layer4 = Conv3D(filters=64, kernel_size=(4, 4, 4), activation='relu')(conv_layer3)
-        pooling_layer2 = MaxPool3D(pool_size=(2, 2, 2))(conv_layer4)
-
-        ## perform batch normalization on the convolution outputs before feeding it to MLP architecture
-        pooling_layer2 = BatchNormalization()(pooling_layer2)
+        pooling_layer2 = BatchNormalization()(pooling_layer1)
         flatten_layer = Flatten()(pooling_layer2)
 
-        ## create an MLP architecture with dense layers : 5120 -> 2048 -> 2
-        ## add dropouts to avoid overfitting / perform regularization
         dense_layer1 = Dense(units=5120, activation='relu')(flatten_layer)
         dense_layer1 = Dropout(0.4)(dense_layer1)
         dense_layer2 = Dense(units=2048, activation='relu')(dense_layer1)
         dense_layer2 = Dropout(0.4)(dense_layer2)
         output_layer2 = Dense(units=2, activation='softmax')(dense_layer2)
-        ## define the model with input layer and output layer
+
         self.model = Model(inputs=input_layer, outputs=output_layer2)
 
     def makeModel(self):
+        filepath="weights-best.hdf5"
+        checkpoint = ModelCheckpoint(filepath, monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
+        callbacks_list = [checkpoint]
         self.model.compile(loss=categorical_crossentropy, optimizer=Adadelta(lr=0.02), metrics=['acc'])
-        self.model.fit(x=self.xtrain, y=self.ytrain, batch_size=1, epochs=20, validation_split=0.1)
+        self.model.fit(x=self.xtrain, y=self.ytrain, batch_size=5, epochs=20, validation_split=0.1, callbacks = callbacks_list,verbose=1)
 
-    def evalModel(self):
+    def evalModel(self,le):
         pred = self.model.predict(self.xtest)
         pred2 = np.argmax(pred, axis=1)
-        print(pred)
-        print(pred2)
-#         print("Accuracy on test set:")
-#         print(accuracy_score(self.ytest,pred))
-              
+        print(le.inverse_transform(pred2))
+
+    def model_load(shape,file):
+        input_layer = Input((shape,64))
+
+        conv_layer1 = Conv3D(filters=8, kernel_size=(4, 4, 4), activation='relu')(input_layer)
+        conv_layer2 = Conv3D(filters=16, kernel_size=(4, 4, 4), activation='relu')(conv_layer1)
+
+        pooling_layer1 = MaxPool3D(pool_size=(2, 2, 2))(conv_layer2)
+
+        pooling_layer2 = BatchNormalization()(pooling_layer1)
+        flatten_layer = Flatten()(pooling_layer2)
+
+        dense_layer1 = Dense(units=5120, activation='relu')(flatten_layer)
+        dense_layer1 = Dropout(0.4)(dense_layer1)
+        dense_layer2 = Dense(units=2048, activation='relu')(dense_layer1)
+        dense_layer2 = Dropout(0.4)(dense_layer2)
+        output_layer2 = Dense(units=2, activation='softmax')(dense_layer2)
+
+        model = Model(inputs=input_layer, outputs=output_layer2)
+        model.load_weights(file)
+        print("Model loaded")
+        print("Model Summary:\n",model.summary())
+        self.model = model
+        return model
+
+    def predict(self,dat,le):
+        self.model = model_load(self.shape)
+        preds = self.model.predict(dat)
+        conf = le.inverse_transform(preds)
+        pred = np.argmax(final, axis=1)
+        return pred,conf
