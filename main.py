16cd4bdbb48340755ca52263f3c56582b6c81b8d
Rohan Datta <rohandatt@gmail.com>
2020-06-01 20:45:47 +0530

    Update main.py
    Added interactive functionality and train test

--- main.py
+++ main.py
@@ -3,23 +3,65 @@ from atfmrdat import FData
 from cnn import FModel
 from datapreproc import Handler
 from optflow import OFMaker
+from nlpext import Reporter
 import os
 import sys
 
-f = FData()
-initdata = f.preprocLoad(shape= (36,36,36),srcdir="RS_data/")
-auxcsv = f.giveAux()
+x = str(input("Do you want to train?(y/n)"))
+if x=="y":
+    s1,s2,s3,s4 = input("Enter shape of images: ").strip().split(" ")
+    sh = (s1,s2,s3,s4)
+    
+    f = FData()
+    initdata = f.preprocLoad(shape= (int(sh[0]),int(sh[1]),int(sh[2])),srcdir="RS_data/")
+    auxcsv = f.giveAux()
+    dp = int(input("Enter no. of datpoints: "))
+    s = Segregator(initdata,dp,128)
+    segdata = s.datselector(auxcsv)
+    s.datManageCheck()
 
-s = Segregator(initdata,4,128)
-segdata = s.datselector(auxcsv)
-s.datManageCheck()
+    of = OFMaker(segdata,128)
+    ofdata = of.optflowit("mirror")
 
-of = OFMaker(segdata,4)
-ofdata = of.optflowit("mirror")
+    dpp = Handler(ofdata,auxcsv,0.2)
+    xtr,xts,ytr,yts = dpp.makeDF()
+    
+    x2 = str(input("Do you want to make a fresh model?(y/n)"))
+    if x2=="y":
+        cnn = FModel(shape = sh,xtrain = xtr,xtest = xts,ytrain = ytr,ytest = yts)
+        cnn.makeModel()
+        cnn.evalModel()
+    elif x2=="n":
+        cnn = FModel(shape = sh,xtrain = xtr,xtest = xts,ytrain = ytr,ytest = yts)
+        cnn.model_load("weights-best.hdf5")
+        cnn.makeModel()
+    else:
+        print("Invalid. Quitting.")
+elif x=="n":
+    srcdir = str(input("Enter source directory of data to be predicted on."))
+    s1,s2,s3,s4 = input("Enter shape of images: ").strip().split(" ")
+    sh = (s1,s2,s3,s4)
+    auxdir = "imtest.csv"
+    f = FData(auxdir)
+    initdata = f.preprocLoad(shape= (int(sh[0]),int(sh[1]),int(sh[2])),srcdir=srcdir)
+    auxcsv = f.giveAux()
+    dp = int(input("Enter no. of datpoints: "))
+    s = Segregator(initdata,dp,128)
+    segdata = s.datselector(auxcsv)
+    s.datManageCheck()
 
-dpp = Handler(ofdata,auxcsv,0.2)
-xtr,xts,ytr,yts = dpp.makeDF()
+    of = OFMaker(segdata,128)
+    ofdata = of.optflowit("mirror")
 
-cnn = FModel(shape = (36,36,36,2),xtrain = xtr,xtest = xts,ytrain = ytr,ytest = yts)
-cnn.makeModel()
-cnn.evalModel()
+    dpp = Handler(ofdata,auxcsv,0.2)
+    dat = dpp.makeTestingDat()
+    dpple = dpp.giveLE()
+    
+    print("Ready to predict")
+    cnn = FModel(shape = sh,xtrain = xtr,xtest = xts,ytrain = ytr,ytest = yts)
+    cnn.model_load("weights-best.hdf5")
+    pred,conf = cnn.predict(dat,dpple)
+    
+    df = ps.read_csv(auxdir)
+    nlp = Reporter(df,pred,dp,conf)
+    nlp.makePDF()
