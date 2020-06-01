d8607fc7e7f805a27b95372cd5b46736204c980f
Rohan Datta <rohandatt@gmail.com>
2020-06-01 20:45:07 +0530

    Update datapreproc.py
    Added Label Encoder export

--- datapreproc.py
+++ datapreproc.py
@@ -5,13 +5,18 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 
 class Handler:
+    
+#######################################################################################
+    
     def __init__(self, data, csv, testsplit, flag = 0):
         self.flag = flag
         self.done = False
         self.data = data
         self.split = testsplit
         self.csv = csv
-        
+       
+#######################################################################################
+    
     def makeDF(self):
         sh = self.data.shape
         total = sh[0]
@@ -21,11 +26,25 @@ class Handler:
         x_ts = self.data[-test_size:]
         y_tr = self.csv["class"][:train_size]
         y_ts = self.csv["class"][-test_size:]
-        letr = LabelEncoder()
-        ly_tr = letr.fit_transform(y_tr)
-        lets = LabelEncoder()
-        ly_ts = lets.fit_transform(y_ts)
+        le = LabelEncoder()
+        ly_tr = le.fit_transform(y_tr)
+        ly_ts = le.transform(y_ts)
+        self.le = le
+        np.save('classes.npy', self.le.classes_)
 #         ly_tr = np.argmax(ly_tr, axis=0)
 #         ly_ts = np.argmax(ly_ts, axis=0)
         return x_tr,x_ts,ly_tr,ly_ts
+      
+#######################################################################################
+    
+    def giveLE(self):
+        encoder = LabelEncoder()
+        encoder.classes_ = np.load('classes.npy')
+        return encoder
+    
+#######################################################################################
+    
+    def makeTestingDat(self):
+        dat = self.data[:]
+        return dat
         
